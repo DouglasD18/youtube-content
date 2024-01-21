e700068886a2d660b8c745d4bfc5cfc343a17a37
Douglinhas <daguiaralcantara@gmail.com>
2024-01-20 23:05:06 -0300

    feat: create update method

--- simple-api/src/model/index.ts
+++ simple-api/src/model/index.ts
@@ -24,4 +24,15 @@ export class BooksModel {
     const books = await this.read();
     return books;
   }
+
+  async update(title: string, book: Book) {
+    const books = await this.read();
+    const index = books.findIndex((book: Book) => book.title === title);
+    if (index === -1) {
+      throw new Error("Book not found");
+    }
+    books[index] = book;
+    await this.write(books);
+  }
+
 }
