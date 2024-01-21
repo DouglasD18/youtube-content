0dda9899584eaddbf783d061bf263fb61daae6f7
Douglinhas <daguiaralcantara@gmail.com>
2024-01-20 22:52:33 -0300

    feat: add router at server

--- simple-api/src/server.ts
+++ simple-api/src/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import router from "./router";
 
 const api = express();
 api.use(express.json());
+api.use(router);
 
 api.listen(3000, () => {
   console.log("Server is running on port 3000!");
