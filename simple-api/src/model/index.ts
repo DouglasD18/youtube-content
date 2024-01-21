134868a063fb4486c61cc50a730cf514e69b0081
Douglinhas <daguiaralcantara@gmail.com>
2024-01-20 22:51:38 -0300

    feat: create BooksModel

---
+++ simple-api/src/model/index.ts
@@ -0,0 +1,22 @@
+import fs from "fs/promises";
+import path from "path";
+import { Book } from "../types";
+
+export class BooksModel {
+  constructor() {}
+
+  private async read() {
+    const books = await fs.readFile(path.join(__dirname, "./books.json"), "utf-8");
+    return JSON.parse(books);
+  }
+
+  private async write(books: any) { 
+    await fs.writeFile(path.join(__dirname, "./books.json"), JSON.stringify(books));
+  }
+
+  async add(book: Book) {
+    const books = await this.read();
+    books.push(book);
+    await this.write(books);
+  }
+}
